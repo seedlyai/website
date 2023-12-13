2d4df4b020958d329764101a3d6de21aaf6b6a6d
Darshil <0.arroyo.sandman@icloud.com>
2023-12-13 14:32:02 -0800

    source

---
+++ src/navigation.js
@@ -0,0 +1,10 @@
+export const headerData = {
+  // ... your data here
+};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/seedlyengine' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://github.com/seedlyai' },
+  ]
+};
